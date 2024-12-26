bafe4a1734b86001692c79002ae81fcbd538f549
irfan-autobits <irfan.autobits@gmail.com>
2024-12-26 18:27:32 +0530

    fps and frame count

--- final-compre/run.py
+++ final-compre/run.py
@@ -44,9 +44,13 @@ print("default camera added",responce)
 
 face_processor = FaceDetectionProcessor(cam_sources, db.session, app)
 
+import time
+
 def send_frame():
     FPS = 1 / 45  # 30 FPS
-    frame_count = 0
+    log_interval = 1
+    frame_count = {cam_name: 0 for cam_name in cam_sources}
+    last_frame_time = {cam_name: time.time() for cam_name in cam_sources}
     """function to send frames to the client from all cameras"""
     try:
         with app.app_context():  # Explicitly create an app context
@@ -54,10 +58,17 @@ def send_frame():
                 with frame_lock:  # Ensure thread-safe access
                     for cam_name, vs in list(vs_list.items()):  # Create a list to avoid runtime changes
                         frame = vs.read()
-                        frame_count += 1
+                        # frame cal
+                        if frame_count[cam_name] % log_interval == 0:
+                            current_time = time.time()
+                            time_diff = current_time - last_frame_time[cam_name]
+                            fps = 1 / time_diff if time_diff > 0 else 0
+                            last_frame_time[cam_name] = current_time                        
+                        frame_count[cam_name] += 1
+
                         if frame is not None:
-                            if frame_count % 30 == 0:
-                                cam_stat_logger.info(f"Processed {frame_count} frames from camera {cam_name}")
+                            if frame_count[cam_name] % log_interval == 0:
+                                cam_stat_logger.info(f"Processed {frame_count[cam_name]} frames from camera {cam_name} at {fps:.2f} FPS")
                             frame = face_processor.process_frame(frame, cam_name)
                             # Encode the frame as JPEG
                             _, buffer = cv2.imencode('.jpg', frame)
@@ -65,8 +76,8 @@ def send_frame():
                             # Emit the frame to the client via WebSocket
                             socketio.emit('frame', {'camera_name': cam_name, 'image': frame_data})
                         else:
-                            if frame_count % 30 == 0:
-                                cam_stat_logger.warning(f"No frame read from camera {cam_name} after {frame_count} attempts")
+                            if frame_count[cam_name] % log_interval == 0:
+                                cam_stat_logger.warning(f"No frame read from camera {cam_name} after {frame_count[cam_name]} attempts at {fps:.2f} FPS")
                             socketio.emit('frame', {'camera_name': cam_name})
                 socketio.sleep(FPS)     
     except Exception as e:
